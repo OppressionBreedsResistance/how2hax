c74f9ea5b97f3a32cc5d1f788e6caa6a4b0086f8
Piotr Kowalczyk <pkowalczyk92@gmail.com>
2021-05-23 17:44:28 +0200

    Added "add", "configure" and "remove" options.

--- h2h-ansible/roles/domain/files/CreateOU.ps1
+++ h2h-ansible/roles/domain/files/CreateOU.ps1
@@ -2,7 +2,12 @@ $ErrorActionPreference = 'SilentlyContinue'
 $OU = "IT", "Interns", "Administrators"
 $PDC = Get-ADDomain | Select-Object -ExpandProperty PDCEmulator
 $DomainDN =Get-ADDomain | Select-Object -ExpandProperty DistinguishedName
-foreach ($OUN in $OU) {
 
-New-ADOrganizationalUnit -Server $PDC -Name $OUN -Path $DomainDN -ProtectedFromAccidentalDeletion $false
+
+foreach ($OUN in $OU) {
+#check if OU exists - create new one if not
+    if ((Get-ADOrganizationalUnit -Filter "Name -eq '$OUN'") -eq $null)
+    {
+        New-ADOrganizationalUnit -Name $OUN -Path $DomainDN -ProtectedFromAccidentalDeletion $false
+    }
 }
--- h2h-ansible/roles/domain/files/CreateUsers.ps1
+++ h2h-ansible/roles/domain/files/CreateUsers.ps1
@@ -1,10 +1,28 @@
 $domain_users = get-content "C:\\ansible_scripts\\users.json" | ConvertFrom-Json | Select-Object -ExpandProperty Users #Get content of user.json file
 $DomainDN =Get-ADDomain | Select-Object -ExpandProperty DistinguishedName
+$Domain = "how2hax.pl"
 foreach ($domain_user in $domain_users){
 
 #Variables definiton
-$username = $domain_user.username
+$name = $domain_user.display_name
+$samAccountName = $domain_user.username
 $password = $domain_user.password
 $department = $domain_user.department
 
+#check if user exists - create new one if not
+if ((Get-ADUser -Filter "SamAccountName -eq '$samAccountName'") -eq $null)
+{
+
+    #check if OU exists - create new one if not
+    if ((Get-ADOrganizationalUnit -Filter "Name -eq '$department'") -eq $null)
+        {
+            New-ADOrganizationalUnit -Name $department -Path $DomainDN -ProtectedFromAccidentalDeletion $false
+        }
+
+    New-ADUser -Name $name -SamAccountName $samAccountName -UserPrincipalName $samAccountName@$Domain -Path "OU=$department,$DomainDN" -AccountPassword(ConvertTo-SecureString $password -AsPlainText -Force) -Enabled $true
+}
+else
+{
+    write-host "User exists"
+}
 }
